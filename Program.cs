6a1284306f0cc14c0edef20282ff4be8347af0e8
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 10:24:44 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Type_Identifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input: a single character
+            Console.WriteLine("Enter a character: ");
+            char inputChar = Convert.ToChar(Console.ReadLine());
+
+            // Check if the character is an alphabet, digit, or special character
+            if ((inputChar >= 'a' && inputChar <= 'z') || (inputChar >= 'A' && inputChar <= 'Z'))
+            {
+                Console.WriteLine($"{inputChar} is an alphabet.");
+            }
+            else if (inputChar >= '0' && inputChar <= '9')
+            {
+                Console.WriteLine($"{inputChar} is a digit.");
+            }
+            else
+            {
+                Console.WriteLine($"{inputChar} is a special character.");
+            }
+        }
+    }
+}
